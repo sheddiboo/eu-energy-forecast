47a42bf5c8d8dba9eb24c99cfe7e8715867ee374
sheddiboo <sheddiboo@gmail.com>
2026-06-01 14:04:39 +0100

    Uncommented Go execution in ingest.py

--- assets/ingest.py
+++ assets/ingest.py
@@ -9,7 +9,7 @@ import os
 
 project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 
-print("Skipping Golang Ingestion to test downstream steps...")
-# subprocess.run("go run main.go parser.go", shell=True, cwd=os.path.join(project_root, "assets", "ingestion", "entsoe_extractor"), check=True)
-# subprocess.run("go run main.go", shell=True, cwd=os.path.join(project_root, "assets", "ingestion", "weather_extractor"), check=True)
-# subprocess.run("go run aws_uploader.go", shell=True, cwd=os.path.join(project_root, "assets", "ingestion"), check=True)
+print("Running Golang Ingestion...")
+subprocess.run("go run main.go parser.go", shell=True, cwd=os.path.join(project_root, "assets", "ingestion", "entsoe_extractor"), check=True)
+subprocess.run("go run main.go", shell=True, cwd=os.path.join(project_root, "assets", "ingestion", "weather_extractor"), check=True)
+subprocess.run("go run aws_uploader.go", shell=True, cwd=os.path.join(project_root, "assets", "ingestion"), check=True)
